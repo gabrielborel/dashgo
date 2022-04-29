475179408b36a027c824d6b3419640e4de09322a
Gabriel Borel <biel_borel@hotmail.com>
2022-04-29 19:44:25 -0300

    ActiveLink component created

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 
@@ -63,6 +64,10 @@ const series = [
 ];
 
 export default function Dashboard() {
+  const [showChart, setShowChart] = useState(false);
+
+  useEffect(() => setShowChart(true), []);
+
   return (
     <Flex direction="column" h="100vh">
       <Header />
@@ -70,26 +75,38 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxWidth={1300} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid
-          flex="1"
-          gap="4"
-          minChildWidth="320px"
-          alignItems="flex-start"
-        >
-          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb={4}>
-            <Text fontSize="lg" mb="4">
-              Inscritos da semana
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
+        {showChart && (
+          <SimpleGrid
+            flex="1"
+            gap="4"
+            minChildWidth="320px"
+            alignItems="flex-start"
+          >
+            <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb={4}>
+              <Text fontSize="lg" mb="4">
+                Inscritos da semana
+              </Text>
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
 
-          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb={4}>
-            <Text fontSize="lg" mb="4">
-              Taxa de abertura
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-        </SimpleGrid>
+            <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb={4}>
+              <Text fontSize="lg" mb="4">
+                Taxa de abertura
+              </Text>
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+          </SimpleGrid>
+        )}
       </Flex>
     </Flex>
   );
